f6be64dea9fc7c41b701635301eb585de9c1b63c
Ramon Imbao <ramonimbao@gmail.com>
2021-06-02 17:57:52 +0800

    Change layout to HHKB

--- config/boards/arm/blerry60/layout.h
+++ config/boards/arm/blerry60/layout.h
@@ -4,4 +4,4 @@
 // #define SPLIT_LEFT_SHIFT
 #define SPLIT_BACKSPACE
 // #define ISO_ENTER			// If commented, ANSI enter. If uncommented, ISO enter.
-#define BOTTOM_7U			// Choose between BOTTOM_625U, BOTTOM_7U, or BOTTOM_HHKB
+#define BOTTOM_HHKB			// Choose between BOTTOM_625U, BOTTOM_7U, or BOTTOM_HHKB
