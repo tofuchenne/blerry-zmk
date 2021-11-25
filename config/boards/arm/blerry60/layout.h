bbbfa8122be0c2df250d81d8c1112ec95dfa978c
Ramon Imbao <ramonimbao@gmail.com>
2021-11-25 21:46:50 +0800

    Modify keymap for HEX.3C

--- config/boards/arm/blerry60/layout.h
+++ config/boards/arm/blerry60/layout.h
@@ -2,6 +2,6 @@
 // Comment or uncomment depending on your needs
 #define SPLIT_RIGHT_SHIFT
 // #define SPLIT_LEFT_SHIFT
-// #define SPLIT_BACKSPACE
+#define SPLIT_BACKSPACE
 // #define ISO_ENTER			// If commented, ANSI enter. If uncommented, ISO enter.
 #define BOTTOM_7U			// Choose between BOTTOM_625U, BOTTOM_7U, or BOTTOM_HHKB
